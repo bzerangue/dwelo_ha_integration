dea6b6bfc7db32f3a1169c535ba93714b672bf42
Brian Zerangue <brian.zerangue@gmail.com>
2025-06-08 16:18:53 -0500

    corrected CONF_EMAIL

--- custom_components/dwelo/__init__.py
+++ custom_components/dwelo/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import logging
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
+from homeassistant.const import CONF_PASSWORD, CONF_EMAIL, Platform
 from homeassistant.core import HomeAssistant
 
 from .const import CONF_GATEWAY_ID, DOMAIN, HOST
@@ -25,7 +25,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     client = DweloClient(
         HOST,
         hass,
-        entry.data[CONF_USERNAME],
+        entry.data[CONF_EMAIL],
         entry.data[CONF_PASSWORD],
         entry.data[CONF_GATEWAY_ID],
     )
